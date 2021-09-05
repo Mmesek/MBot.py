e8b3da18d9a74654f5a2c4aae2be01e6c26f054a
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-09-05 21:50:53 +0200

    Added autoremoving archived threads from last_message cache

--- bot/dispatch/threads.py
+++ bot/dispatch/threads.py
@@ -3,3 +3,8 @@ from MFramework import onDispatch, Bot, Channel
 @onDispatch
 async def thread_create(self: Bot, data: Channel):
     self.cache[data.guild_id].threads[data.id] = data.parent_id
+
+@onDispatch
+async def thread_update(self: Bot, data: Channel):
+    if data.thread_metadata.archived:
+        self.cache[data.guild_id].last_messages.pop(data.id, None)
