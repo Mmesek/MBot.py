e24ea8c6748394f183ae9d112df1314ff61c665c
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-09-28 23:32:11 +0200

    Renamed base Cache to Collection aka Fixed import

--- bot/dispatch/messages.py
+++ bot/dispatch/messages.py
@@ -15,6 +15,6 @@ async def message_create(self: Bot, data: Message):
 async def direct_message_create(self: Bot, data: Message):
     await self.cache[self.primary_guild].logging["direct_message"](data)
     if data.channel_id not in self.cache[0]:
-        from MFramework.database.cache import Cache
-        self.cache[data.guild_id][data.channel_id] = Cache()
+        from MFramework.database.cache_internal.models import Collection
+        self.cache[data.guild_id][data.channel_id] = Collection()
     self.cache[data.guild_id][data.channel_id].store(data)
