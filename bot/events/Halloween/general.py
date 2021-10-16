a0f83faf402a75a952a3d99009596114af265076
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-10-16 16:48:00 +0200

    Change displaying counts to ones based on count query

--- bot/events/Halloween/general.py
+++ bot/events/Halloween/general.py
@@ -479,7 +479,7 @@ async def history(ctx: Context, user: User = None, limit: int = 10) -> Embed:
 
     turns = []
     turn_history = s.query(HalloweenLog).filter(HalloweenLog.server_id == ctx.guild_id, HalloweenLog.target_id == user.id).order_by(HalloweenLog.timestamp.desc()).limit(limit).all()
-    turn_count = 0
+    turn_count = s.query(sa.func.count(HalloweenLog.target_id)).filter(HalloweenLog.server_id == ctx.guild_id, HalloweenLog.target_id == user.id, HalloweenLog.previous != HalloweenLog.race).all()
     for entry in turn_history[:int(limit)]:
         _u = entry.user_id
         _u = ctx.cache.members.get(int(_u), Guild_Member(user=User(username=_u))).user.username
@@ -490,17 +490,15 @@ async def history(ctx: Context, user: User = None, limit: int = 10) -> Embed:
                 line = f"Drank potion and became `{entry.race}`"
             else:
                 line = f"Enlisted as `{entry.race}`"
-            turn_count += 1
         elif entry.previous in IMMUNE_TABLE and entry.race in HUNTERS:
             line = f"Convinced to fight as `{entry.race}`"
-            turn_count += 1
         else:
             line = f"`{entry.previous}` into `{entry.race}`"
-            turn_count += 1
         turns.append(f"[<t:{int(entry.timestamp.timestamp())}:d>] {line} by `{_u}`")
 
     targets = []
     targets_history = s.query(HalloweenLog).filter(HalloweenLog.server_id == ctx.guild_id, HalloweenLog.user_id == user.id, HalloweenLog.user_id != HalloweenLog.target_id).order_by(HalloweenLog.timestamp.desc()).limit(limit).all()
+    targets_count = s.query(sa.func.count(HalloweenLog.user_id)).filter(HalloweenLog.server_id == ctx.guild_id, HalloweenLog.user_id == user.id, HalloweenLog.user_id != HalloweenLog.target_id).all()
     for entry in targets_history[:int(limit)]:
         _u = entry.target_id
         _u = ctx.cache.members.get(int(_u), Guild_Member(user=User(username=_u))).user.username
@@ -517,7 +515,7 @@ async def history(ctx: Context, user: User = None, limit: int = 10) -> Embed:
         e.addField("Turns", "\n".join(turns))
     if targets:
         e.addField("Targets", "\n".join(targets))
-    e.addField("Statistics", f"Total Turns: `{turn_count}`\nTotal Targets: `{len(targets_history)}`")
+    e.addField("Statistics", f"Total Turns: `{turn_count}`\nTotal Targets: `{targets_count}`")
     return [e]
 
 @register(group=Groups.GLOBAL, main=halloween)
