de8a7fcc18aeaec347e3fd4da1102625165c0aab
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-11-06 14:16:27 +0100

    Fix showing unique players in summary & Add unique victims

--- bot/events/Halloween/general.py
+++ bot/events/Halloween/general.py
@@ -765,7 +765,10 @@ async def summary(ctx: Context):
         HalloweenLog.previous != HalloweenLog.race,
         HalloweenLog.race == Race.Human
     ).first()
-    players = s.query(sa.func.count(Halloween.server_id)).filter(
+    players = s.query(sa.func.count(sa.distinct(HalloweenLog.user_id))).filter(
+        Halloween.server_id == ctx.guild_id
+    ).first()
+    unique_victims = s.query(sa.func.count(Halloween.server_id)).filter(
         Halloween.server_id == ctx.guild_id
     ).first()
     turns = s.query(sa.func.count(HalloweenLog.server_id)).filter(
@@ -782,7 +785,7 @@ async def summary(ctx: Context):
         Race.Enchanter: "Enchanters"
     }
 
-    stats = [("Players",players[0]), ("Turns", turns[0]), ("Bites", bites[0]), ("Cures", cures[0])]
+    stats = [("Players",players[0]), ("Victims",unique_victims), ("Turns", turns[0]), ("Bites", bites[0]), ("Cures", cures[0])]
     e.addField("Total", "\n".join([f"`{i[0]:10}`: `{i[1]:>5}`" for i in stats]))
     total = sorted(Halloween.get_total(s, ctx.guild_id).items(), key=lambda x: x[1], reverse=True)
     total = "\n".join([f"`{PLURALS.get(i[0], i[0]):10}`: `{i[1]:>5}`" for i in total])
