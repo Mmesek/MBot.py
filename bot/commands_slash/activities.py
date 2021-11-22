d5fe6cb0453b45ccca7a0476c93e3514c899c5fb
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-11-22 01:21:41 +0100

    Add command to start activity in voice channel

---
+++ bot/commands_slash/activities.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+from MFramework import register, Groups, Context, Channel_Types, Invite_Target_Types
+from MFramework.commands._utils import ChannelType
+
+class Activities(Enum):
+    Watch_Together = 880218394199220334
+    Poker_Night = 755827207812677713
+    Betrayal = 773336526917861400
+    Fishington = 814288819477020702
+    Chess = 832012774040141894
+    Checkers = 832013003968348200
+    Sketchy_Artist = 879864070101172255
+    Awkword = 879863881349087252
+    Putts = 832012854282158180
+    Doodle_Crew = 878067389634314250
+    Letter_Tile = 879863686565621790
+    World_Snacks = 879863976006127627
+    SpellCast = 852509694341283871
+
+
+@register(group=Groups.NITRO)
+async def activity(ctx: Context, channel: ChannelType[Channel_Types.GUILD_VOICE.value], activity: Activities) -> str:
+    '''
+    Start activity in selected voice channel
+    Params
+    ------
+    channel:
+        voice channel to start activity in
+    activity:
+        activity to start (Not all them might work)
+    '''
+    invite = await ctx.bot.create_channel_invite(channel.id, target_type=Invite_Target_Types.EMBEDDED_APPLICATION, target_application_id=activity.value)
+    return f"[Click here to join {invite.target_application.name} in {invite.channel.name}!](<https://discord.gg/{invite.code}>)"
