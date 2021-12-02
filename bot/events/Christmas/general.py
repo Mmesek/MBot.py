8fb9ee4515e08293351396c889eaab757d3f5700
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-12-03 00:03:21 +0100

    Update/Fix Christmas minigame & Add  leaderboards for snowball/presents

--- bot/events/Christmas/general.py
+++ bot/events/Christmas/general.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone, timedelta
-from MFramework import Context, User, Groups, register, Event, EventBetween, Cooldown
-from MFramework.database import alchemy as db
+from MFramework import Context, User, Groups, register, Event, EventBetween
+from MFramework.commands.cooldowns import CacheCooldown, cooldown
+from ... import database as db
 
 def _t(key, language='en', **kwargs):
     from mlib.localization import tr
@@ -8,12 +9,12 @@ def _t(key, language='en', **kwargs):
 
 @register(group=Groups.GLOBAL)
 @Event(month=12)
-async def Christmas(ctx: Context):
+async def christmas(ctx: Context):
     '''Christmas Event commands'''
     pass
 
-@register(group=Groups.GLOBAL, main=Christmas)
-@Cooldown(hours=2)
+@register(group=Groups.GLOBAL, main=christmas)
+@cooldown(hours=2, logic=CacheCooldown)
 async def gift(ctx: Context, user: User, *, language):
     '''Send specified user a gift'''
     if user.id == ctx.user.id:
@@ -25,38 +26,39 @@ async def gift(ctx: Context, user: User, *, language):
     gift_type = db.types.Item.Gift
     own_present = False
 
-    user_history = db.Log.filter(s, server_id=ctx.guild_id, user_id=user, ByUser=ctx.user.id, type=gift_type).first()
+    #user_history = db.Log.filter(s, server_id=ctx.guild_id, user_id=user, ByUser=ctx.user.id, type=gift_type).first()
 
-    if user_history is not None:
-        return await ctx.reply(_t('present_already_sent', language, timestamp=user_history.Timestamp.strftime("%Y/%m/%d %H:%M")))
+    #if user_history is not None:
+    #    return await ctx.reply(_t('present_already_sent', language, timestamp=user_history.Timestamp.strftime("%Y/%m/%d %H:%M")))
 
-    for item in this_user.Items:
-        if 'Present' in item.Item.Name and item.Item.Name != 'Golden Present':
-            if item.Quantity > 0:
+    for item in this_user.items:
+        if 'Presents' == item.item.name and item.item.name != 'Golden Present':
+            if item.quantity > 0:
                 own_present = True
             break
 
     if not own_present:
         return await ctx.reply(_t('not_enough_presents', language))
 
-    golden_present = db.Items.by_name(s, "Golden Present")
+    golden_present = db.items.Item.by_name(s, "Golden Present")
 
-    last_gift = s.query(db.Log).filter(db.Log.server_id == ctx.guild_id, db.Log.user_id == ctx.user.id, db.Log.type == gift_type).order_by(db.Log.timestamp.desc()).first()
+    last_gift = None# s.query(db.Log).filter(db.Log.server_id == ctx.guild_id, db.Log.user_id == ctx.user.id, db.Log.type == gift_type).order_by(db.Log.timestamp.desc()).first()
     now = datetime.now(tz=timezone.utc)
 
     if last_gift is None or (now - last_gift.timestamp) >= timedelta(hours=2):
-        target_user = db.User.fetch_or_add(s, id=user)
+        target_user = db.User.fetch_or_add(s, id=user.id)
 
         gift = db.Inventory(golden_present, 1)
-        send_item = db.Inventory(item.Item)
+        send_item = db.Inventory(item.item)
         this_user.transfer(ctx.guild_id, target_user, [send_item], [gift], turn_item=True)
+        this_user.claim_items(ctx.guild_id, [db.Inventory(db.items.Item.by_name(s, "Sent Present"))])
         s.commit()
-        await ctx.reply(_t('present_sent_successfully', language))        
+        await ctx.reply(_t('present_sent_successfully', language, user=user.username))        
     else:
         await ctx.reply(_t('remaining_cooldown', language, cooldown=timedelta(hours=2) - (now - last_gift.Timestamp)))
 
 
-@register(group=Groups.GLOBAL, main=Christmas)
+@register(group=Groups.GLOBAL, main=christmas)
 async def cookie(ctx: Context, user: User, *, language):
     '''Send specified user a cookie'''
     if user.id == ctx.user.id:
@@ -67,14 +69,14 @@ async def cookie(ctx: Context, user: User, *, language):
     this_user = db.User.fetch_or_add(s, id=ctx.user_id)
     recipent_user = db.User.fetch_or_add(s, id=user.id)
 
-    inv = db.Inventory(db.Item("Cookie", db.Items.Cookie))
+    inv = db.Inventory(db.items.Item.fetch_or_add(s, name="Cookie", type="Gift"))
     transaction = this_user.transfer(ctx.guild_id, recipent_user, [inv], remove_item=False)
 
     s.add(transaction)
     s.commit()
     await ctx.reply(_t('cookie_sent', language))
 
-@register(group=Groups.GLOBAL, main=Christmas)
+@register(group=Groups.GLOBAL, main=christmas)
 @EventBetween(after_month=12, before_day=24)
 async def advent(ctx: Context, *, language):
     '''Claim today's Advent'''
@@ -85,7 +87,7 @@ async def advent(ctx: Context, *, language):
     if today.month != 12 or today.day > 24:
         return await ctx.reply(_t('advent_finished', language))
 
-    advent_type = db.Items.Advent
+    advent_type = db.items.Items.Advent
     _today = datetime(today.year, today.month, today.day, tzinfo=timezone.utc)
     _year = datetime(today.year, 1, 1)
     claimed_total = s.query(db.Log).filter(db.Log.user_id == ctx.user.id, db.Log.type == "Advent", db.Log.timestamp >= _year).all()
@@ -95,7 +97,7 @@ async def advent(ctx: Context, *, language):
             claimed_today = True
 
     if not claimed_today:
-        advent_item = db.Item('Advent', advent_type)
+        advent_item = db.items.Item('Advent', advent_type)
         advent_inventory = db.Inventory(advent_item)
         this_user.claim_items(ctx.guild_id, [advent_inventory])
         s.commit()
@@ -103,7 +105,7 @@ async def advent(ctx: Context, *, language):
     else:
         await ctx.reply(_t('advent_already_claimed', language))
 
-@register(group=Groups.GLOBAL, main=Christmas)
+@register(group=Groups.GLOBAL, main=christmas, private_response=True)
 async def hat(ctx: Context, user: User):
     '''Adds Santa's hat onto user's avatar'''
     await ctx.deferred()
@@ -117,4 +119,4 @@ async def hat(ctx: Context, user: User):
     buffered = BytesIO()
     img.save(buffered, format="PNG")
     img_str = buffered.getvalue()
-    await ctx.send(file=img_str, filename="avatar.png")
+    await ctx.bot.create_message(ctx.channel_id, file=img_str, filename="avatar.png")
--- bot/events/Christmas/snowballs.py
+++ bot/events/Christmas/snowballs.py
@@ -1,7 +1,7 @@
 from random import SystemRandom as random
 
 from MFramework import Context, Groups, register, Event, User, Embed
-from MFramework.commands.cooldowns import cooldown
+from MFramework.commands.cooldowns import cooldown, CacheCooldown
 from ...database import items, models, types
 
 from MFramework.commands._utils import Error
@@ -21,7 +21,7 @@ async def snowball(ctx: Context) -> Embed:
 
 @register(group=Groups.GLOBAL, main=snowball)
 @Event(month=12)
-@cooldown(minutes=3)
+@cooldown(minutes=3, logic=CacheCooldown)
 async def throw(ctx: Context, target: User):
     '''
     Throw a snowball at someone!
@@ -33,35 +33,52 @@ async def throw(ctx: Context, target: User):
     s = ctx.db.sql.session()
     user = models.User.fetch_or_add(s, id=ctx.user_id)
     snowballs = next(filter(lambda x: x.item.name == "Snowball", user.items), None)
-    if not snowballs:
+    if not snowballs or snowballs.quantity == 0:
         raise SnowballError("Sorry, you don't have any snowballs! Gather some firstly!")
 
     msgs = await ctx.bot.get_channel_messages(ctx.channel_id, limit=5)
     user_msg = next(filter(lambda x: x.author.id == target.id, msgs), None)
     if user_msg:
+        target_user = models.User.fetch_or_add(s, id=target.id)
+
         item = items.Inventory(items.Item.by_name(s, "Snowball"))
-        transaction = user.transfer(ctx.guild_id, target.id, [item]) 
+        send_item = items.Inventory(item.item)
+        if random().randint(1, 100) < 90:
+            splashed = [items.Inventory(items.Item.by_name(s, "Splashed Snowball"), 1)]
+            success = True
+            user.claim_items(ctx.guild_id, [items.Inventory(items.Item.by_name(s, "Thrown Snowball"))])
+        else:
+            success = False
+            splashed = None
+            user.claim_items(ctx.guild_id, [items.Inventory(items.Item.by_name(s, "Missed Snowball"))])
+
+        #transaction = user.transfer(ctx.guild_id, target.id, [item]) 
+        transaction = user.transfer(ctx.guild_id, target_user, [send_item], splashed)
         #FIXME: Right now it only transfers snowball,
         # adding a method that "turns" item in transit (Sent A, Received B)
         # would be a possible solution for this [There was something similiar for gifting 2020]
         s.add(transaction)
-
-        return f"<@{ctx.user_id}>, You have hit <@{target.id}>!"
+        s.commit()
+        if success:
+            return f"<@{ctx.user_id}>, You have hit <@{target.id}>!"
+        else:
+            return f"<@{ctx.user_id}>, you missed!"
     return SnowballError("Sorry, Provided user is out of range")
 
 @register(group=Groups.GLOBAL, main=snowball)
 @Event(month=12)
-@cooldown(hours=1)
+@cooldown(hours=1, logic=CacheCooldown)
 async def gather(ctx: Context):
     '''Gather snowballs to throw at someone later!'''
     s = ctx.db.sql.session()
     user = models.User.fetch_or_add(s, id=ctx.user_id)
 
     amount = random().randint(2,5)
-    item = items.Item.fetch_or_add(s, name="Snowball", type=types.Item.Resource)
+    item = items.Item.fetch_or_add(s, name="Snowball", type=types.Item.Event)
     snowball_inventory = items.Inventory(item, amount)
 
     transaction = user.claim_items(ctx.guild_id, [snowball_inventory])
     s.add(transaction)
+    s.commit()
 
     return f"<@{ctx.user_id}>, Successfully gathered {amount} snowballs!"
