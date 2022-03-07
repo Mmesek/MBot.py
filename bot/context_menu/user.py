94797920cb580b34e1bb83b547909793d82f8669
Mmesek <13630781+Mmesek@users.noreply.github.com>
2022-03-07 17:27:09 +0100

    Add Context Menu Warn option

--- bot/context_menu/user.py
+++ bot/context_menu/user.py
@@ -38,3 +38,15 @@ async def Experience(ctx: Context, user: User):
     await ctx.deferred(private=True)
     from ..commands_slash.leaderboards import exp
     return await exp(ctx, user)
+
+@register(group=Groups.MODERATOR, auto_defer=False, name="Warn")
+async def Warn(ctx: Context, user: User):
+    '''
+    Warn user
+    Params
+    ------
+    user:
+        user to warn
+    '''
+    from ..commands_slash.infractions import Reason, Row, TextInput
+    return Reason(Row(TextInput("Reason", placeholder="Reason of this action")), title="Infraction", custom_id=f"Warn-{user.id}")
