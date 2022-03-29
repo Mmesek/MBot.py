6f967af2268950c4a11ae46fbf690bd688f7c5e0
Mmesek <13630781+Mmesek@users.noreply.github.com>
2022-03-29 02:36:06 +0200

    Update to use all files in bot & bump pyython ver

--- mbot.sh
+++ mbot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$( cd "$( dirname "$0" )" && pwd )"
-/usr/bin/screen -dm -S "MFramework" /usr/bin/python3.7 -m MFramework bot/dispatch bot/commands_slash --cfg=data/secrets.ini --log=INFO
+/usr/bin/screen -dm -S "MFramework" /usr/bin/python3.9 -m MFramework bot --cfg=data/secrets.ini --log=INFO
