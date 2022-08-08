8cea08a8d04c5fd126559e8c205294412b4a71bb
Mmesek <13630781+Mmesek@users.noreply.github.com>
2022-08-08 22:15:43 +0200

    Add stats & leaderboard commands

--- bot/events/arena.py
+++ bot/events/arena.py
@@ -2,7 +2,8 @@ from datetime import datetime, timedelta, timezone
 
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
-from MFramework import Context, Groups, register
+from MFramework import Context, Embed, Groups, register
+from MFramework.utils.leaderboards import Leaderboard, Leaderboard_Entry
 from mlib.database import Base
 
 
@@ -161,3 +162,65 @@ async def bonus(ctx: Context, bonus: int, user_id: int = None, *, session=None):
     session.commit()
 
     return player.bonus
+
+
+@register(group=Groups.GLOBAL, main=arena)
+async def stats(ctx: Context, user_id: int = None, *, session=None):
+    """
+    Shows player stats
+    Params
+    ------
+    user_id:
+        user to show
+    """
+    if not session:
+        session = ctx.db.sql.session()
+    player = session.query(Gladiator).filter(Gladiator.id == (user_id or ctx.user_id)).first()
+    if not player:
+        return "No stats"
+    embed = Embed()
+    embed.add_field("Current damage bonus", str(player.bonus), True)
+    if player.history:
+        embed.add_field("Total damage dealt", str(sum([i.damage for i in player.history])), True)
+        embed.add_field("Last attack", str(player.history[-1].timestamp) + " UTC", True)
+        remaining_cooldown = timedelta(minutes=30) - (datetime.now(tz=timezone.utc) - player.history[-1].timestamp)
+        if remaining_cooldown.total_seconds() > 0:
+            embed.add_field("Cooldown remaining", str(remaining_cooldown), True)
+    return embed
+
+
+@register(group=Groups.GLOBAL, main=arena)
+async def leaderboard(ctx: Context, *, session=None):
+    """
+    Shows leaderboards
+    """
+    return Leaderboard(ctx, ctx.user_id, [Leaderboard_Entry(ctx, i[0], i[1]) for i in await list_users(ctx)]).as_embed()
+
+
+@register(group=Groups.GLOBAL, main=arena)
+async def list_bosses(ctx: Context):
+    """
+    Shows list of boss's and their stats
+    """
+    session = ctx.db.sql.session()
+    bosses: list[Gladiator_Boss] = session.query(Gladiator_Boss).order_by(Gladiator_Boss.ends_at).all()
+    return "\n".join([f"{boss.name} - {boss.health} | {boss.ends_at}" for boss in bosses])
+
+
+async def list_users(ctx: Context, min_damage: int = 1, *, session=None):
+    """
+    Lists users that dealt any damage
+    Params
+    ------
+    min_damage:
+        minimum damage user dealt
+    """
+    if not session:
+        session = ctx.db.sql.session()
+    u: list[Gladiator_History] = (
+        session.query(Gladiator_History.user_id, sa.func.sum(Gladiator_History.damage))
+        .group_by(Gladiator_History.user_id)
+        .order_by(sa.func.sum(Gladiator_History.damage))
+        .all()
+    )
+    return [i for i in u if i[1] >= min_damage]
