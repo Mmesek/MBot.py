de2453e48af240f2c6226f8c88914282c5cc875a
Mmesek <13630781+Mmesek@users.noreply.github.com>
2023-02-13 14:00:36 +0100

    Add sending valentines
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- bot/events/Valentines/general.py
+++ bot/events/Valentines/general.py
@@ -1,8 +1,10 @@
 import enum
 import functools
 
-from MFramework import Context, Embed, Event, Groups, User, register
+from MFramework import Allowed_Mentions, Context, Embed, Event, Groups, User, register
 from MFramework.commands.cooldowns import CacheCooldown, cooldown
+
+from ... import database as db
 
 
 def _t(key, language="en", **kwargs):
@@ -28,6 +30,74 @@ def valentines(cls=None, *, should_register: bool = True):
     if cls:
         return i(cls)
     return i
+
+
+@register(group=Groups.GLOBAL, main=valentines, private_response=True)
+@Event(month=2, day=14)
+@cooldown(minutes=10, logic=CacheCooldown)
+async def valentine(ctx: Context, user: User, message: str) -> Embed:
+    """
+    Send a secret valentine to another user!
+    Params
+    ------
+    user:
+        User you want to send valentine to
+    message:
+        Message you want to include in your valentine
+    """
+    if user.id == ctx.user.id:
+        return "You can't send a secret valentine to yourself! :("
+
+    await send(ctx, user, "Secret Valentine")
+
+    await ctx.send(
+        f"<@{user.id}>, you have received a valentine!",
+        embeds=Embed(title="❤", description=message, color=int("e76f71", 16)),
+        allowed_mentions=Allowed_Mentions(users=[user.id]),
+        channel_id=ctx.channel_id,
+    )
+
+    return "Valentine sent!"
+
+
+class Valentine(enum.Enum):
+    Hug = "Hug"
+    Kiss = "Kiss"
+    Valentine = "Valentine"
+
+
+@register(group=Groups.GLOBAL, main=valentines)
+@Event(month=2, day=14)
+@cooldown(minutes=10, logic=CacheCooldown)
+async def send(ctx: Context, user: User, valentine: Valentine) -> Embed:
+    """
+    Send Hug or kisses to another user!
+    Params
+    ------
+    user:
+        User to send to
+    valentine:
+        Valentine to send
+    """
+    if type(valentine) != str:
+        value = valentine.value
+    else:
+        value = valentine
+
+    if user.id == ctx.user.id:
+        return f"You can't send a {value} to yourself! :("
+
+    s = ctx.db.sql.session()
+
+    this_user = db.User.fetch_or_add(s, id=ctx.user_id)
+    recipent_user = db.User.fetch_or_add(s, id=user.id)
+
+    inv = db.Inventory(db.items.Item.fetch_or_add(s, name=value, type="Gift"))
+    transaction = this_user.transfer(ctx.guild_id, recipent_user, [inv], remove_item=False)
+
+    s.add(transaction)
+    s.commit()
+    return f"{ctx.user.username} has sent a {value} to <@{user.id}>!"
 
 
 def compatibility(user_id_a, user_id_b) -> float:
