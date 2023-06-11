ce3be4d1743af3ad4ef9ca88a5bc8781e393c5c2
DaCockIsCoding <35774093+DaCockIsCoding@users.noreply.github.com>
2023-06-11 15:57:27 +0300

    Add canvas drawing functionality.
        Similar to r/place or Blurple event
        The code now allows users to create and manipulate a canvas.
        Users can use the /place command to place pixels of different colors on the canvas once a minute.
        The /show command can be used to display the entire canvas or a zoomed-in portion of it.
        The /colorcodes command shows the available color codes for pixel placement.

---
+++ bot/events/place.py
@@ -0,0 +1,155 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mlib.graphing import create_image
+import json
+import os
+
+from MFramework.commands.cooldowns import CacheCooldown, cooldown
+from MFramework import Attachment, Context, Groups, register
+
+# Canvas Size
+CANVAS_WIDTH = 300
+CANVAS_HEIGHT = 300
+BORDER_SIZE = 5
+# Default zoom when using the show command, will show a 50x50 pixel image
+DEFAULT_ZOOM_SIZE = 50
+
+
+# color codes, list is expandable
+COLOR_CODES = {
+    "red": (255, 0, 0),
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255),
+    "yellow": (255, 255, 0),
+    "magenta": (255, 0, 255),
+    "cyan": (0, 255, 255),
+    "black": (0, 0, 0),
+    "white": (255, 255, 255),
+    "gray": (128, 128, 128),
+    "maroon": (128, 0, 0),
+    "olive": (128, 128, 0),
+    "navy": (0, 0, 128),
+    "purple": (128, 0, 128),
+    "teal": (0, 128, 128),
+    "lime": (0, 128, 0),
+    "aqua": (0, 255, 255),
+    "silver": (192, 192, 192),
+    "fuchsia": (255, 0, 255),
+    "orange": (255, 165, 0),
+    "pink": (255, 192, 203),
+    "gold": (255, 215, 0),
+    "brown": (165, 42, 42),
+    "indigo": (75, 0, 130),
+    "violet": (238, 130, 238),
+    "turquoise": (64, 224, 208),
+    "coral": (255, 127, 80),
+    "khaki": (240, 230, 140),
+    "plum": (221, 160, 221),
+    "salmon": (250, 128, 114),
+    "orchid": (218, 112, 214),
+    "tan": (210, 180, 140),
+}
+
+canvas = np.zeros((CANVAS_HEIGHT, CANVAS_WIDTH, 3), dtype=np.uint8)
+
+
+def display_canvas(canvas):
+    plt.figure(figsize=(CANVAS_WIDTH / 100, CANVAS_HEIGHT / 100), dpi=100)
+    plt.imshow(canvas)
+    plt.axis("off")
+    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+    plt.savefig("canvas.png", bbox_inches="tight", pad_inches=0)
+    return create_image(plt)
+
+
+def save_canvas():
+    with open("canvas.json", "w") as f:
+        json.dump(canvas.tolist(), f)
+
+
+def load_canvas():
+    global canvas
+    try:
+        if os.path.isfile("canvas.json"):
+            with open("canvas.json", "r") as f:
+                canvas = np.array(json.load(f))
+                canvas = np.asarray(canvas, dtype=np.uint8)
+        else:
+            print("Canvas file not found. Creating a new canvas.")
+    except Exception as e:
+        print(f"An error occurred while loading the canvas: {e}")
+
+
+load_canvas()
+
+
+@register(group=Groups.GLOBAL)
+async def show(
+    ctx: Context, x: int = None, y: int = None, size: int = DEFAULT_ZOOM_SIZE
+):
+    """ "
+    Show the canvas or a zoomed-in portion of the canvas.
+    Params
+    ------
+     x:
+        The x coordinate of the center of the zoomed-in area (optional).
+     y:
+        The y coordinate of the center of the zoomed-in area (optional).
+     size:
+        The size of the zoomed-in area (optional). Defaults to 50.
+
+    Example
+    -------
+        /show
+        /show 10 20
+        /show 10 20 5
+    """
+    if x is None or y is None:
+        image = canvas
+    else:
+        x1 = max(0, x - size // 2)
+        x2 = min(CANVAS_WIDTH, x + size // 2)
+        y1 = max(0, y - size // 2)
+        y2 = min(CANVAS_HEIGHT, y + size // 2)
+        image = canvas[y1:y2, x1:x2]
+
+    var = display_canvas(image)
+    return Attachment(file=var, filename="canvas.png")
+
+
+@register(group=Groups.GLOBAL)
+@cooldown(minutes=1, logic=CacheCooldown)
+async def place(ctx: Context, x: int, y: int, color_code: str):
+    """
+    Place a pixel on the canvas at the specified coordinates with the specified color.
+    Params
+    ------
+    x:
+        The x coordinate of the pixel.
+    y:
+        The y coordinate of the pixel.
+    color_code:
+        The color code representing the desired color.
+     Example
+     -------
+        /place 10 20 red
+    """
+    if 0 <= x < CANVAS_WIDTH and 0 <= y < CANVAS_HEIGHT:
+        color_code = color_code.lower()
+        if color_code in COLOR_CODES:
+            r, g, b = COLOR_CODES[color_code]
+            canvas[y, x] = (r, g, b)
+            save_canvas()
+            await ctx.reply(f"Pixel placed at ({x}, {y}) with color {color_code}")
+        else:
+            await ctx.reply(
+                "Invalid color code! Use the `colorcodes` command to see the available color codes."
+            )
+    else:
+        await ctx.reply("Invalid coordinates!")
+
+
+@register(group=Groups.GLOBAL)
+async def colorcodes(ctx: Context):
+    color_code_list = "\n".join(COLOR_CODES.keys())
+    await ctx.reply(f"Available color codes:\n```{color_code_list}```")
