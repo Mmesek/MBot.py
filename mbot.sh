2f30cf1d675cdb73794be3bad1eafd747725ae1e
Mmesek <13630781+Mmesek@users.noreply.github.com>
2023-06-24 13:21:51 +0200

    Remove unused startup scripts
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- mbot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd "$( cd "$( dirname "$0" )" && pwd )"
-/usr/bin/screen -dm -S "MFramework" /usr/bin/python3.9 -m MFramework bot --cfg=data/secrets.ini --log=INFO
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i -e 's/_init_params,/_init_params + ["**kwargs"],/g' /usr/local/lib/python3.10/dataclasses.py
-python -m MFramework bot --log=DEBUG
