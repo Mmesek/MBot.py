5857263ec3f158dc6116adc0a1a3c8c2b899d70e
Mmesek <13630781+Mmesek@users.noreply.github.com>
2023-12-22 17:51:08 +0100

    Move task initialization to cache
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -1,22 +1,6 @@
 from MFramework import onDispatch
-from MFramework.database.alchemy import (  # noqa: F401
-    Channel,
-    Role,
-    Server,
-    Subscription,
-    Webhook,
-)
 from sqlalchemy.orm import Query, Session  # noqa: F401
 
 from . import types  # noqa: F401
-from .items import Drop, Event, Inventory, Items, Location  # noqa: F401
-from .log import Activity, Log, Presence, Statistic, Transaction  # noqa: F401
-from .models import Snippet, Task, User  # noqa: F401
-from .rp import Character, Skill  # noqa: F401
-
-
-@onDispatch(priority=101)
-async def guild_create(bot, guild):
-    from ..utils.scheduler import add_guild_tasks
-
-    add_guild_tasks(bot, guild.id)
+from . import alchemy
+from .models import Channel, Role, Server, Snippet, Task, User  # noqa: F401
