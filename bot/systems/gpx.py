cfceb0add839dc3417b9a048e063f11e61a2c463
Mmesek <13630781+Mmesek@users.noreply.github.com>
2025-01-27 01:59:49 +0100

    Add dispatch action to extract stats from GPX attachment
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

---
+++ bot/systems/gpx.py
@@ -0,0 +1,38 @@
+import time
+
+import gpxpy
+import httpx
+from MFramework import Bot, Embed, Message, Message_Reference, onDispatch
+
+
+@onDispatch(predicate=lambda x: x.attachments and x.guild_id == 463433273620824104)
+async def message_create(bot: Bot, data: Message):
+    if not any(a.filename.endswith("gpx") for a in data.attachments):
+        return
+    s = time.time()
+    embeds = []
+    for attachment in data.attachments:
+        if attachment.filename.endswith("gpx"):
+            async with httpx.AsyncClient() as client:
+                r = await client.get(attachment.url)
+            gpx = gpxpy.parse(r.content)
+            gpx.smooth(True, True, True)
+            gpx.smooth()
+            m = gpx.get_moving_data()
+            avg_speed = m.moving_distance / m.moving_time
+
+            embeds.append(
+                Embed(title=attachment.filename, timestamp=gpx.time, url=attachment.url)
+                .add_field("Duratioin", f"{m.moving_time / 60:.2f} m", True)
+                .add_field("Distance", f"{m.moving_distance / 1000:.2f}km", True)
+                .add_field("Speed m/s (Avg/Best)", f"{avg_speed:.2f}/{m.max_speed:.2f} m/s", True)
+                .add_field(
+                    "Tempo min/km (Avg/Best)",
+                    f"{1000 / (avg_speed * 60):.2f}/{1000 / (m.max_speed * 60):.2f}",
+                    True,
+                )
+            )
+    e = time.time()
+    await bot.create_message(
+        data.channel_id, f"Processed in {e - s:.2}s", embeds=embeds, message_reference=Message_Reference(data.id)
+    )
